97d9b684e5e34948f99f3f8c923980ea1b426096
jayteealao <jayteealao@gmail.com>
2026-07-18 22:45:59 +0100

    build: bump version to 1.2.0-alpha.03

--- isometric-compose/build.gradle.kts
+++ isometric-compose/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "io.github.jayteealao"
-version = "1.2.0-alpha.03-SNAPSHOT"
+version = "1.2.0-alpha.03"
 
 android {
     namespace = "io.github.jayteealao.isometric.compose"
--- isometric-core/build.gradle.kts
+++ isometric-core/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "io.github.jayteealao"
-version = "1.2.0-alpha.03-SNAPSHOT"
+version = "1.2.0-alpha.03"
 
 kotlin {
     jvmToolchain(17)
